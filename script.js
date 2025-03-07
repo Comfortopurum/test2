26cbff79b64fd32e67f87ad98204783c42086368
comfort <104990635+Comfortopurum@users.noreply.github.com>
2025-03-07 22:03:47 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-main = document.querySelector('[main-container]');
-const pay = document.querySelector('[pay-now]');
-
-// notification toast eventListener
-pay.addEventListener('click', function () {
-  main.classList.add('closed');
-});
